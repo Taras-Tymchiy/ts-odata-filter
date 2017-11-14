d5f00a839316ae8e538b9cf80c99272d2dc901cb
Taras Tymchiy <Taras.Tymchiy@eleks.com>
2017-11-15 01:12:27 +0200

    added object-path-proxy

---
+++ src/object-path-proxy.ts
@@ -0,0 +1,27 @@
+
+export type ObjPathProxy<TRoot, T> = {
+  [P in keyof T]: ObjPathProxy<TRoot, T[P]>;
+};
+
+const pathSymbol = Symbol('Object path');
+
+export class ObjectPathProxy {
+  static create<T>(path: string[] = []): ObjPathProxy<T, T> {
+    const proxy = new Proxy({[pathSymbol]: path}, {
+      get (target, key) {
+        return key === pathSymbol
+          ? target[pathSymbol]
+          : ObjectPathProxy.create([...(path || []), key.toString()]);
+      }
+    });
+    return proxy as any as ObjPathProxy<T, T>;
+  }
+
+  static getPath<TRoot, T>(obj: ObjPathProxy<TRoot, T>): string[] {
+    return (obj as any)[pathSymbol];
+  }
+
+  static isProxy<TRoot, T>(value: any): value is ObjPathProxy<TRoot, T> {
+    return value && typeof value === 'object' && !!ObjectPathProxy.getPath<TRoot, T>(value as ObjPathProxy<TRoot, T>);
+  }
+}
